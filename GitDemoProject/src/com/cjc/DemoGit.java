55d4d9cf87edd8998f61cc2047fda2989ed73f2f
chima <chima@Ashu>
2024-08-02 12:02:26 +0530

    by ashvini

---
+++ GitDemoProject/src/com/cjc/DemoGit.java
@@ -0,0 +1,22 @@
+package com.cjc;
+
+public class DemoGit {
+	public void m1() {
+		System.out.println("m1");
+	}
+	public void m2() {
+		System.out.println("m2");
+	}
+	public void m3() {
+		System.out.println("m3");
+	}
+	public void m4() {
+		System.out.println("m4");
+	}
+	public void m5() {
+		System.out.println("m5");
+	}
+public static void main(String[] args) {
+	
+}
+}
